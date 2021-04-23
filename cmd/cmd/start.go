6969bf55d8096d9d1f1edd1c3c0473d380c2eb39
Adrian Riobo Lorenzo <ariobolo@redhat.com>
2021-04-23 09:15:29 +0200

    Use viper for flags on start subcommand

--- cmd/cmd/start.go
+++ cmd/cmd/start.go
@@ -1,19 +1,32 @@
 package cmd
 
 import (
-	"github.com/adrianriobo/qe-eventmanager/pkg/logging"
+	"strings"
+
+	"github.com/go-stomp/stomp/v3"
 	"github.com/spf13/cobra"
 	"github.com/spf13/pflag"
+	"github.com/spf13/viper"
+
+	"github.com/adrianriobo/qe-eventmanager/pkg/logging"
+	"github.com/adrianriobo/qe-eventmanager/pkg/messaging"
+)
+
+const (
+	flagBrokers         string = "brokers"
+	flagCertificateFile string = "certificate-file"
+	privateKeyFile      string = "private-key-file"
+	caCerts             string = "ca-certs"
 )
 
 func init() {
 	rootCmd.AddCommand(startCmd)
 
 	flagSet := pflag.NewFlagSet("start", pflag.ExitOnError)
-	// flagSet.StringArrayP("brokers", "b", nil, "list of brokers acting on failover")
-	// flagSet.StringP("certificate-file", "c", "", "certificate file for client auth")
-	// flagSet.StringP("certificate-file", "c", "", "certificate file for client auth")
-	// flagSet.StringP("certificate-file", "c", "", "certificate file for client auth")
+	flagSet.StringP(flagBrokers, "b", "", "list of brokers acting on failover")
+	flagSet.StringP(flagCertificateFile, "", "", "certificate file for client auth")
+	flagSet.StringP(privateKeyFile, "", "", "key file for client auth")
+	flagSet.StringP(caCerts, "", "", "root ca for messageing server auth")
 
 	startCmd.Flags().AddFlagSet(flagSet)
 }
@@ -23,6 +36,9 @@ var startCmd = &cobra.Command{
 	Short: "Start event manager",
 	Long:  "Start event manager",
 	RunE: func(cmd *cobra.Command, args []string) error {
+		if err := viper.BindPFlags(cmd.Flags()); err != nil {
+			return err
+		}
 		if err := runStart(); err != nil {
 			return err
 		}
@@ -35,6 +51,21 @@ func runStart() error {
 		return err
 	}
 	logging.Infof("Testing the start")
+	connection := messaging.NewUMBConnection(
+		viper.GetString(flagCertificateFile),
+		viper.GetString(privateKeyFile),
+		viper.GetString(privateKeyFile),
+		strings.Split(viper.GetString(flagBrokers), ","))
+	if err := connection.Connect(); err != nil {
+		return err
+	}
+	sub, _ := connection.FailoverSubscribe("Consumer.psi-crcqe-openstack.test5.VirtualTopic.qe.ci.product-scenario.crcqe.test4",
+		stomp.AckAuto)
+	msg, err := sub.Read()
+	if err == nil {
+		logging.Debugf(string(msg.Body[:]))
+	}
+	connection.Disconnect()
 	return nil
 }
 
--- go.mod
+++ go.mod
@@ -8,6 +8,7 @@ require (
 	github.com/sirupsen/logrus v1.8.1
 	github.com/spf13/cobra v1.1.3
 	github.com/spf13/pflag v1.0.5
+	github.com/spf13/viper v1.7.0
 	golang.org/x/crypto v0.0.0-20210220033148-5ea612d1eb83
 	google.golang.org/appengine v1.6.5
 	k8s.io/component-base v0.21.0
