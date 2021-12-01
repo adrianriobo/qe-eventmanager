b98e873ad2d66db2e8c1f2d1e88dd0467784cec2
Adrian Riobo Lorenzo <ariobolo@redhat.com>
2021-12-01 12:21:13 +0100

    Move pipelines manage results to common pkg

---
+++ pkg/util/pipelines/pipelines.go
@@ -0,0 +1,41 @@
+package pipelines
+
+import (
+	"github.com/adrianriobo/qe-eventmanager/pkg/util/http"
+	"github.com/adrianriobo/qe-eventmanager/pkg/util/logging"
+	"github.com/adrianriobo/qe-eventmanager/pkg/util/xunit"
+	"github.com/tektoncd/pipeline/pkg/apis/pipeline/v1beta1"
+)
+
+const (
+	resultStatusPassed string = "passed"
+	resultStatusFailed string = "failed"
+)
+
+// TODO make general vailable
+func GetResultValue(results []v1beta1.PipelineRunResult, resultParamID string) string {
+	for _, result := range results {
+		if result.Name == resultParamID {
+			return result.Value
+		}
+	}
+	return ""
+}
+
+// TODO this should be moved to result parameter from the pipeline
+func GetResultState(url string) string {
+	file, err := http.GetFile(url)
+	if err != nil {
+		logging.Error(err)
+		return ""
+	}
+	count, err := xunit.CountFailures(file)
+	if err != nil {
+		logging.Error(err)
+		return ""
+	}
+	if count == 0 {
+		return resultStatusPassed
+	}
+	return resultStatusFailed
+}
