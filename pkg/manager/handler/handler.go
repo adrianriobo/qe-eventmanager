08c87adc0431213119125f28c4dd53ac729ff1b0
Adrian Riobo Lorenzo <ariobolo@redhat.com>
2022-04-29 15:42:36 +0200

    Initial rule structure with handler for filters

---
+++ pkg/manager/handler/handler.go
@@ -0,0 +1,24 @@
+package handler
+
+import (
+	"github.com/adrianriobo/qe-eventmanager/pkg/util/logging"
+	"github.com/spyzhov/ajson"
+)
+
+func matchFilters(event string, filters []string) (bool, error) {
+	json := []byte(event)
+	root, _ := ajson.Unmarshal(json)
+	for _, filter := range filters {
+		node, err := root.JSONPath(filter)
+		if err != nil {
+			logging.Error("Error with %v", err)
+			return false, err
+		}
+		if len(node) == 0 {
+			logging.Error("Error event does not match the filters")
+			return false, nil
+		}
+		logging.Debug("Found event marching the filters")
+	}
+	return true, nil
+}
