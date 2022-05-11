cd47c8795d243cbce7f01b0d84b710e147c6d561
Adrian Riobo Lorenzo <ariobolo@redhat.com>
2022-05-11 18:55:01 +0200

    added file based params for configuration to image entrypoint

--- images/builder/entrypoint.sh
+++ images/builder/entrypoint.sh
@@ -2,35 +2,15 @@
 
 VALID_CONFIG=true
 # Check required ENVs
-if [ -z "${BROKERS}" ]; then 
-  echo "BROKERS ENV is required"
+if [ -z "${PROVIDERS_FILE_PATH}" ]; then 
+  echo "PROVIDERS_FILE_PATH is required"
   VALID_CONFIG=false
 fi
 
-if [ -z "${CA}" ]; then 
-  echo "CA ENV is required"
+if [ -z "${FLOWS_FILE_PATH}" ]; then 
+  echo "FLOWS_FILE_PATH ENV is required"
   VALID_CONFIG=false  
 fi
-
-if [ -z "${CERTIFICATE}" ]; then 
-  echo "CERTIFICATE ENV is required"
-  VALID_CONFIG=false
-fi
-
-if [ -z "${KEY}" ]; then 
-  echo "KEY ENV is required"
-  VALID_CONFIG=false
-fi
-
-if [ -z "${CONSUMER_ID}" ]; then 
-  echo "CONSUMER_ID ENV is required"
-  VALID_CONFIG=false
-fi
-
-if [ -z "${DRIVER}" ]; then 
-  DRIVER=stomp
-fi
-
 
 if [ "${VALID_CONFIG}" = false ]; then
   echo "Add the required ENVs"
@@ -38,21 +18,6 @@ if [ "${VALID_CONFIG}" = false ]; then
 fi
 
 # Run qe-eventmanager
-if [ -z "${KUBECONFIG}" ]; then
-  exec qe-eventmanager start \
-    --brokers "${BROKERS}" \
-    --ca-certs "${CA}" \
- 		--certificate-file "${CERTIFICATE}" \
-		--private-key-file "${KEY}" \
-    --consumerid "${CONSUMER_ID}" \
-    --driver "${DRIVER}" 
-else 
-  exec qe-eventmanager start \
-    --brokers "${BROKERS}" \
-    --ca-certs "${CA}" \
-    --certificate-file "${CERTIFICATE}" \
-    --private-key-file "${KEY}" \
-    --kubeconfig "${KUBECONFIG}" \
-    --consumerid "${CONSUMER_ID}" \
-    --driver "${DRIVER}" 
-fi
+exec qe-eventmanager start \
+    --providers-filepath "${PROVIDERS_FILE_PATH}" \
+    --flows-filepath "${FLOWS_FILE_PATH}"
